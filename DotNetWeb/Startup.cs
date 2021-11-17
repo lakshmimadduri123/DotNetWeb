3cfbb0f0ec4bf0b657371c9928ec0adc9c42390e
Sreevidya-1 <sreevidyam323@gmail.com>
2021-11-17 09:42:12 +0530

    updated hello vidya

--- DotNetWeb/Startup.cs
+++ DotNetWeb/Startup.cs
@@ -33,6 +33,7 @@ namespace DotNetWeb
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello vidya!");
                 });
             });
         }
